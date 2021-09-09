44a5277675991812dc864ca73326af0830c4c460
Leonarch <arcomat@virgilio.it>
2021-09-09 18:05:06 +0200

    Java Update

---
+++ exercise-6.js
@@ -0,0 +1,23 @@
+function getKeys(obj) {
+  console.log(obj);
+  let newKeys = []; 
+  for( element in obj){
+    
+  newKeys.push(element);
+    
+  }
+
+
+return newKeys;
+}
+
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 32,
+  city: 'Rome',
+  job: 'Developer',
+};
+
+const keys = getKeys(person);
+console.log(keys);
